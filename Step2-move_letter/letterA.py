f66f50b8981afa13c9ce4657a4510dca5431c822
simonc8 <33280951+simonc8@users.noreply.github.com>
2018-06-04 13:38:42 +0100

    letterA

--- Step2-move_letter/letterA.py
+++ Step2-move_letter/letterA.py
@@ -1,39 +1,19 @@
-# A typical game loop looks a bit like this:
-# while game_has_not_ended():
-#    process_input()
-#    update()
-#    draw()
-
-# draw()
-#    Called by Pygame Zero when it needs to redraw your game window.
-#    draw() must take no arguments.
-
-#Pygame Zero attempts to work out when the game screen needs to be redrawn to avoid redrawing if nothing has changed. On each step of the game loop it will draw the screen in the following situations:
-
-#    If you have defined an update() function (see below).
-#    If a clock event fires.
-#    If an input event has been triggered.
-# Don't attempt to modify or animate within the draw() function - use the update() function
-
-# update() or update(dt)
-
-#    Called by Pygame Zero to step your game logic. This will be called repeatedly, 60 times a second, unless
-#    the function is called with parameter dt which is the update time in seconds.
-
+# Set the size of the window by making numerical variables WIDTH and HEIGHT
 WIDTH = 640
 HEIGHT = 480
 
+# Make numerical variables x and y and give them initial values.
 x = WIDTH/2
 y = HEIGHT/2
 
 
+# Define function update where the x coordinate will be changed
 def update():
     global x
-    #x += 1
+    x = x + 1
 
+# Define function draw which will draw the letter on the screen
 def draw():
-    screen.clear()
-    screen.draw.text('A', center=(x, y), color=(128,0,128), fontsize=24)
-    screen.draw.text('A', center=(x-20, y), color=(128,0,128))
+    screen.draw.text('A', center=(x, y))
 
 
